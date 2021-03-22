529219ce8b5c9705f6126027bde8f9b8a400af53
Jordi Castellanos <jordi.castellanos2002@gmail.com>
2021-03-22 20:10:27 +0000

    Working on q5

--- QuizQ5.cpp
+++ QuizQ5.cpp
@@ -7,4 +7,20 @@ int main()
   int upperRange;
   int i;
   int flag;
+
+  if(lowerRange > upperRange)
+  {
+    cout<<"Bruh"<<;
+    return 0;
+  }
+  while (lowerRange<upperRange)
+  {
+    flag = 0;
+
+    for(i=2; i <= lowerRange/2;i++)
+    {
+      if(lowerRange%i ==0)
+      
+    }
+  }
 }
