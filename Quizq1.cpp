aef37719d1383dbed25bad6ffa1baade99c04a13
Jordi Castellanos <jordi.castellanos2002@gmail.com>
2021-03-22 20:46:49 +0000

    working on q1

---
+++ Quizq1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+  int lowerRange;
+  int upperRange;
+  int1 = rand()%100;
+  int2 = rand()%100;
+  int3 = rand()%100;
+  int4 = rand()%100;
+  int5 = rand()%100;
+  
+if(int1 < int2)
+{
+  cout<<int2<<endl;
+}
+
+
+  
+}
