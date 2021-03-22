bf3d2afb753d25a1709c7a3622a82db5db658466
Jordi Castellanos <jordi.castellanos2002@gmail.com>
2021-03-22 20:54:10 +0000

    Finished Q1

--- Quizq1.cpp
+++ Quizq1.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 #include <iomanip>
+#include <time.h>
 using namespace std;
 
 int main()
 {
-  int lowerRange;
-  int upperRange;
-  int1 = rand()%100;
-  int2 = rand()%100;
-  int3 = rand()%100;
-  int4 = rand()%100;
-  int5 = rand()%100;
-  
+  srand(time(NULL));
+  int int1 = rand()%100;
+  int int2 = rand()%100;
+  int int3 = rand()%100;
+  int int4 = rand()%100;
+  int int5 = rand()%100;
+  cout<<"The  numbers are "<<int1<<" "<<int2<<" "<<int3<<" "<<int4<<" "<<int5<<endl;
 if(int1 < int2)
 {
   cout<<int2<<endl;
 }
-
+if(int2<int3)
+{
+  cout<<int3<<endl;
+}
+if(int3<int4)
+{
+  cout<<int4<<endl;
+}
+if(int4<int5)
+{
+  cout<<int5<<endl;
+}
 
   
 }
