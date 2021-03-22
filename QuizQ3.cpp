06a3cc6155a9ccd0b6d77a758981176160185a9a
Jordi Castellanos <jordi.castellanos2002@gmail.com>
2021-03-22 20:27:21 +0000

    Working on q3

---
+++ QuizQ3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int lowerRange;
+  int upperRange;
+  
+  cout<<"Enter the range, starting from lower range, from 0 to 99.\n";
+  cin>>lowerRange>>upperRange;
+  
+  if((lowerRange>99||upperRange>99)||(lowerRange<0||upperRange<0))
+  {
+      cout<<"Your ranges do not lie within the bounds.";
+  }
+
+  
+}
