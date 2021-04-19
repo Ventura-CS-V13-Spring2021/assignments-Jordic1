e95821d2ed5d16ea662429f780f7fc252146565c
Jordi Castellanos <>
2021-04-19 19:23:24 +0000

    working on q1

---
+++ midtermq1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+std::ifstream infile("students.txt");
+int count = 0;
+
+int main()
+{
+
+    
+
+}
+
+void fileRead(void)
+{
+  ifstream myfile;
+  myfile.open ("students.txt");
+  while(std::getline(infile,line))
+  {
+    std::istringstream iss(line)
+    if(count%2)
+    {
+        
+    }
+  }
+  myfile.close();
+}
