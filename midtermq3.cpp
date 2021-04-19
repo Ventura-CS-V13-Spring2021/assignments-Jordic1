f9a2805aaf4824725b49c2080d8918bd36b79c80
Jordi Castellanos <>
2021-04-19 18:40:45 +0000

    Finished I think

--- midtermq3.cpp
+++ midtermq3.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
 #include <random>
+#include <fstream>
 using namespace std;
-
+int randint;
+int min;
+int max;
+int difference;
 int main()
 {
-
+    int num1,num2,num3;
+    getRdnum();
+    num1 = randint;
+    getRdnum();
+    num2 = randint;
+    getRdnum();
+    num3 = randint;
+    
+    findMax(num1, num2, num3);
+    findMin(num1, num2, num3);
+    getDifference();
+    fileWrite();
+    
 }
 
 getRdnum()
@@ -12,3 +28,46 @@ getRdnum()
     randint = rand()%10;
 }
 
+findMin(int n1, int n2, int n3)
+{
+    if(n1<n2 && n1<n3)
+    {
+        min = n1;
+    } else if(n2<n3 && n2<n1)
+    {
+        min = n2;
+    }else
+    {
+        min = n3;
+    }
+    cout<<"The min is "<< min << endl;
+}
+
+findMax(int n1, int n2, int n3)
+{
+    if(n1>n2 && n1>n3)
+    {
+        max = n1;
+    } else if(n2>n3 && n2>n1)
+    {
+        max = n2;
+    }else
+    {
+        max = n3;
+    }
+    cout<<"The max is : "<< max << endl;
+}
+
+getDifference()
+{
+    difference = (max - min);
+    cout<<"The difference is : "<< difference << endl;
+}
+
+fileWrite()
+{
+  ofstream myfile;
+  myfile.open ("question3.txt");
+  myfile <<difference<<endl;
+  myfile.close();
+}
