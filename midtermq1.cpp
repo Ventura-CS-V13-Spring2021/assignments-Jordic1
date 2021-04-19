bb07517f9fc95e06cd1a626df34223eca67ca6d3
Jordi Castellanos <>
2021-04-19 19:35:34 +0000

    finishing up q1

--- midtermq1.cpp
+++ midtermq1.cpp
@@ -2,8 +2,10 @@
 #include <fstream>
 using namespace std;
 std::ifstream infile("students.txt");
-int count = 0;
-
+int count = 1;
+int a,b;
+double avg;
+int sum;
 int main()
 {
 
@@ -18,10 +20,27 @@ void fileRead(void)
   while(std::getline(infile,line))
   {
     std::istringstream iss(line)
-    if(count%2)
+    if(count%2==0)
     {
-        
+        while(infile>>a>>b)
+        {
+            summation(a,b);
+            average(a,b);
+            
+            cout<<" Score 1 : "<< a << " Score 2 : " << b << " Sum : " << sum << " Average : " << avg << endl;
+        }
+    }else
+    {
+        println;
     }
   }
   myfile.close();
+}
+int summation(int n1, int n2)
+{
+    sum = n1 + n2;
+}
+double average(int n1, int n2)
+{
+    avg = sum/2;
 }
