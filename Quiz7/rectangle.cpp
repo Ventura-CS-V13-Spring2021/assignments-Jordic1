1414857ede6fce86bb321b7d535fa4de4d64ec63
Jordi Castellanos <>
2021-05-10 21:07:35 +0000

    Working on setting and printing rectangle

--- Quiz7/rectangle.cpp
+++ Quiz7/rectangle.cpp
@@ -7,6 +7,11 @@ Coordinate Rectangle::getRT(){
   return rt;
 }
 double Rectangle::getArea(){
-  double area = ((rt.getX()-lb.getX())/2
+  double area = ((rt.getX()-lb.getX()) * (rt.getY()-lb.getY)));
   return area;
 }
+Coordinate Coordinate::getCenter(){
+  double first = (lb.getX()+(rt.getX())-rt.getX()))/2;
+  double second = (lb.getY()+(rt.getY())-rt.getY()))/2;
+}
+
