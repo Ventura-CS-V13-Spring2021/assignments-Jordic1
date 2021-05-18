e094eea2fda9b0a6404289d36b7827aa92d34120
Jordi Castellanos <>
2021-05-18 21:15:54 +0000

    Setting up a user input so i can test.

--- Final/Q3.cpp
+++ Final/Q3.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 using namespace std;
 
-class Student {
+class Student 
+{
 	string name;
 	int numClasses;
 	string* classList;
+
+public:
+  Student():name(), numClasses(0), classList(NULL){};
+
+  Student(string s, int num)
+  {
+    name = s;
+    numClasses = num;
+    ClassList = new string[num];
+
+    for(int i = 0; i < numClasses; i++)
+    {
+      cout<<"Course name : "; //for testing
+      cin >> classList[i];
+    }
+  }
 }
